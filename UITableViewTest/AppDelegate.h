6916aeb432634e90555b3a7c41326fa652947513
iBlock <a58@58deiMac.local>
2014-08-28 16:04:22 +0800

    Initial Commit

---
+++ UITableViewTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITableViewTest
+//
+//  Created by iBlock on 14-8-28.
+//  Copyright (c) 2014年 iBlock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UITableViewTest
+//
+//  Created by iBlock on 14-8-28.
+//  Copyright (c) 2014年 iBlock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
